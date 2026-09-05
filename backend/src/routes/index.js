712ffef736d4047a4d8b7142e9e551491276af70
Deep Pathak <ideeppathak96@gmail.com>
2026-09-05 15:17:37 +0530

    Solved Conflict

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,14 +4,14 @@ import authRoutes from "./auth.js";
 import employeeRoutes from "./employees.js";
 import contractRoutes from "./contracts.js";
 import attendanceRoutes from "./attendance.js";
-<<<<<<< HEAD
+
 import timeOffRoutes from "./timeOff.js";
 import payslipRoutes from "./payslips.js";
-=======
+
 import salaryRulesRoutes from "./salaryRules.js";
 import salaryStructuresRoutes from "./salaryStructures.js";
 import timeOffRoutes from "./timeOff.js";
->>>>>>> 8ba959926afb716741b9f4604edb81c03bb6e81b
+
 
 const router = express.Router();
 
@@ -30,23 +30,21 @@ router.use("/contracts", contractRoutes);
 // Attendance routes
 router.use("/attendance", attendanceRoutes);
 
-<<<<<<< HEAD
+
 // Time Off routes (/api/v1/time-off)
 router.use("/time-off", timeOffRoutes);
 
 // Payslip routes (/api/v1/payslips)
 router.use("/payslips", payslipRoutes);
 
-export default router;
-=======
+
+
 // Salary Rule routes (/api/v1/salary-rules)
 router.use("/salary-rules", salaryRulesRoutes);
 
 // Salary Structure routes (/api/v1/salary-structures)
 router.use("/salary-structures", salaryStructuresRoutes);
 
-// Time Off routes (/api/v1/time-off)
-router.use("/time-off", timeOffRoutes);
 
 export default router;
->>>>>>> 8ba959926afb716741b9f4604edb81c03bb6e81b
+
